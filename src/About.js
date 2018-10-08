33688add3264cee1759ca4a0aa441681308063d8
Ru Nacken <ru@chunk.nl>
2018-10-08 18:43:42 +0200

    added routing

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+            this is about
+            </div>
+            );
+    }
+}
+export default About;
